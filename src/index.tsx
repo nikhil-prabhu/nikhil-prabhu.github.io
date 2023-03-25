1edc43d02681fff45e6f6355c899a08d06c38ece
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2023-03-25 17:22:33 +0530

    feat: Use custom theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider, ThemeConfig } from "antd";
+
+const theme: ThemeConfig = {
+  token: {
+    colorPrimary: "#000000",
+    borderRadius: 0,
+  },
+  algorithm: undefined,
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={theme}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
